52086c13967adc9e16d487a42a5b351d22443251
rades98 <reznicek98@seznam.cz>
2022-04-12 07:44:44 -0700

    Added database with seed and migrations

---
+++ src/infrastructure/PersistenceLayer/Database/Configuration/DbConfiguration.cs
@@ -0,0 +1,37 @@
+﻿namespace PersistenceLayer.Database.Configuration
+{
+    using DomainLayer.Entities.Product;
+    using Microsoft.EntityFrameworkCore;
+    using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+    internal class DbConfigurations : IEntityTypeConfiguration<ProductEntity>
+    {
+        public void Configure(EntityTypeBuilder<ProductEntity> builder)
+        {
+            builder.ToTable("Products");
+            builder.HasKey(product => product.Id);
+
+            builder.Property(product => product.Id)
+                .IsRequired()
+                .ValueGeneratedOnAdd();
+
+            builder.Property(product => product.Created)
+                .IsRequired();
+
+            builder.Property(product => product.Name)
+                .HasMaxLength(50)
+                .IsRequired();
+
+            builder.Property(product => product.Price)
+                .HasPrecision(12, 2)
+                .IsRequired();
+
+            builder.Property(product => product.ImgUri)
+                .IsRequired()
+                .HasConversion(uriIn => uriIn.ToString(), utiOut => new Uri(utiOut));
+
+            builder.Property(product => product.Description)
+                .HasMaxLength(250);
+        }
+    }
+}
---
+++ src/presentation/API/Registrations/ServiceRegistration.cs
@@ -0,0 +1,14 @@
+﻿using PersistenceLayer;
+
+namespace API.Registrations
+{
+    public static class ServiceRegistration
+    {
+        public static IServiceCollection AddServices(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.RegisterDatabase(configuration);
+
+            return services;
+        }
+    }
+}
