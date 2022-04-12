2d93cb0de58cab7e2009fd6f92839743ca47232e
rades98 <reznicek98@seznam.cz>
2022-04-12 06:24:10 -0700

    #1 Added entities

---
+++ src/core/DomainLayer/Entities/AuditableEntity.cs
@@ -0,0 +1,10 @@
+﻿namespace DomainLayer.Entities
+{
+    public class AuditableEntity
+    {
+        public Guid Id { get; set; }
+        public DateTime Created { get; set; }
+        public DateTime? Changed { get; set; }
+        public DateTime? Deleted { get; set; }
+    }
+}
---
+++ src/core/DomainLayer/Entities/Product/ProductEntity.cs
@@ -0,0 +1,10 @@
+﻿namespace DomainLayer.Entities.Product
+{
+    public class ProductEntity : AuditableEntity
+    {
+        public Uri ImgUri { get; set; }
+        public decimal Price { get; set; }
+        public string? Description { get; set; }
+        public string Name { get; set; }
+    }
+}
