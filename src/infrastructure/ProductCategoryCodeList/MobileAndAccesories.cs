4232344a3184202c0c42bd699947c3b98f2f904c
Radek Řezníček <radek.reznicek@alza.cz>
2022-05-26 12:46:17 +0200

    Revert "1st part"
    This reverts commit 2f8507ca61cc9ca5059150b677b2a0ea65b4205d.

--- src/infrastructure/ProductCategoryCodeList/MobileAndAccesories.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace ProductCategoryCodeList
-{
-    public static class MobileAndAccesories
-    {
-        public static readonly Guid MobilePhonesId = new("CE94AE71-1DD0-40F4-A89B-CF70BA3D9E3B");
-        public static readonly Guid CasesId = new("A6312A0B-30C2-40B5-A55A-39C2203F301A");
-        public static readonly Guid AdaptersId = new("8B1DB412-7D98-4CD0-BAD6-4A7C70B235B1");
-    }
-}
--- src/infrastructure/ProductCategoryCodeList/ProductCategories.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace ProductCategoryCodeList
-{
-    public static class ProductCategories
-    {
-        public static readonly Guid EshopId = new("689BEEC2-0592-46FE-8FBE-12EBCE0A458B");
-        public static readonly Guid MobAndAccId = new("689BEEC2-0592-46FE-8FBE-12EBCE0A458B");
-        public static readonly Guid PcAndAccId = new("2F3A0726-B1D8-49FC-BD31-79AD33F3BFDE");
-    }
-}
