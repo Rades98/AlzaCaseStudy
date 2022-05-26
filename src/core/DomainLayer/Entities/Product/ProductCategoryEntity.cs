2f8507ca61cc9ca5059150b677b2a0ea65b4205d
Radek Řezníček <radek.reznicek@alza.cz>
2022-05-26 12:39:13 +0200

    1st part

---
+++ src/core/DomainLayer/Entities/Product/ProductCategoryEntity.cs
@@ -0,0 +1,12 @@
+﻿namespace DomainLayer.Entities.Product
+{
+    public class ProductCategoryEntity : AuditableEntity
+    {
+        public string Name { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public Guid? ParentProductCategoryId { get; set; }
+        public ProductCategoryEntity? ParentProductCategory { get; set; } = new();
+        public IEnumerable<ProductEntity>? Products { get; set; }
+        public IEnumerable<ProductCategoryRelationEntity>? ProductCategoryRelations { get; set; }
+    }
+}
---
+++ src/infrastructure/PersistenceLayer/Database/Configuration/Products/ProductCategoryConfiguration.cs
@@ -0,0 +1,34 @@
+﻿namespace PersistenceLayer.Database.Configuration.Products
+{
+    using DomainLayer.Entities.Product;
+    using Microsoft.EntityFrameworkCore;
+    using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+    internal class ProductCategoryConfiguration : IEntityTypeConfiguration<ProductCategoryEntity>
+    {
+        public void Configure(EntityTypeBuilder<ProductCategoryEntity> builder)
+        {
+        //    builder.ToTable("ProductCategories");
+        //    builder.HasKey(productCategory => productCategory.Id);
+
+        //    builder.Property(productCategory => productCategory.Id)
+        //        .IsRequired()
+        //        .ValueGeneratedOnAdd();
+
+        //    builder.Property(productCategory => productCategory.Created)
+        //        .IsRequired();
+
+        //    builder.Property(productCategory => productCategory.Name)
+        //        .HasMaxLength(50)
+        //        .IsRequired();
+
+        //    builder.Property(productCategory => productCategory.Description)
+        //        .HasMaxLength(250);
+
+        //    builder
+        //        .HasOne(productCategory => productCategory.ParentProductCategory)
+        //        .WithMany()
+        //        .HasForeignKey(productCategory => productCategory.ParentProductCategoryId);
+        }
+    }
+}
